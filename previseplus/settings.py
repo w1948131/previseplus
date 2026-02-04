991c442480a280b7455b85c03127e5f7011329c0
UM40 <143409957+UM40@users.noreply.github.com>
2026-02-04 12:50:55 +0000

    feat: allowed hosts for render domain

--- previseplus/settings.py
+++ previseplus/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-44$5-8+40@(#hewm!olgp2!=%&x=dd7zy&1ne%yk#ob5+wwf!=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
