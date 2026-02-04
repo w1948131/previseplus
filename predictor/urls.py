d440cbb3701ba3ece3b9ba864624ac7fbec5d431
UM40 <143409957+UM40@users.noreply.github.com>
2026-02-04 01:44:33 +0000

    feat: Added Ticker Info page

--- predictor/urls.py
+++ predictor/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path("", views.home, name = "home"),
     path("dashboard/", views.dashboard, name = "dashboard"),
     path("predict/<str:ticker_value>/<int:number_of_days>/", views.predict, name="predict"),
-    path("ticker/", views.ticker, name="ticker"),
+    path("tickers/", views.ticker, name="tickers"),
     path("search/", views.search, name="search")
     
 ]
