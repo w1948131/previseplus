390fdd3baff10b70e07cbe1d36fd9472b6a05b4f
UM40 <143409957+UM40@users.noreply.github.com>
2026-02-04 13:16:14 +0000

    feat: settings debug

--- previseplus/settings.py
+++ previseplus/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY") #render key
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG") == "True"
 
 ALLOWED_HOSTS = ["*"]
 
